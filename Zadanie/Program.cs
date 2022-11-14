d81a655022881c2997d1d4e95eb150989a1a8b6f
Denis <dfazylov@mail.ru>
2022-11-14 17:03:14 +0500

    Final

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -2,8 +2,8 @@
 {
 string[] array1 = new string[5] {"privet", "75", "87665", "kak_dela", "123"};
 string[] array2 = new string[array1.Length];
-PrintArray(array2);
 MakeSecondArray(array1, array2);
+PrintArray(array2);
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
