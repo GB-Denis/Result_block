6bf21cfb6ed1043ded330437fbb14b61d43e7e85
Denis <dfazylov@mail.ru>
2022-11-14 16:47:06 +0500

    void_MakeSecondArray

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -3,6 +3,7 @@
 string[] array1 = new string[5] {"privet", "75", "87665", "kak_dela", "123"};
 string[] array2 = new string[array1.Length];
 PrintArray(array2);
+MakeSecondArray(array1, array2);
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -10,6 +11,18 @@ void PrintArray(string[] array)
         Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
+}
+void MakeSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
 }
 }
 Zadanie_block_result();
