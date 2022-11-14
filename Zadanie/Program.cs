e1b0310fef92c67418dd24867ee293734c5e0991
Denis <dfazylov@mail.ru>
2022-11-14 16:43:51 +0500

    void_print_array

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -2,5 +2,14 @@
 {
 string[] array1 = new string[5] {"privet", "75", "87665", "kak_dela", "123"};
 string[] array2 = new string[array1.Length];
+PrintArray(array2);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 }
 Zadanie_block_result();
