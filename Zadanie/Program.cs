de0044a6b9338161ac580b63b043a1457f24fad2
Denis <dfazylov@mail.ru>
2022-11-14 16:42:10 +0500

    void_zadanie_block_result

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -1,2 +1,6 @@
-﻿string[] array1 = new string[5] {"privet", "75", "87665", "kak_dela", "123"};
+﻿void Zadanie_block_result()
+{
+string[] array1 = new string[5] {"privet", "75", "87665", "kak_dela", "123"};
 string[] array2 = new string[array1.Length];
+}
+Zadanie_block_result();
